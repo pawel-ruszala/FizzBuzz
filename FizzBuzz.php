e85bf4a298a64f6cb5dc6d617719909e1dcb6b71
pawel-ruszala93 <pawel.ruszala93@gmail.com>
2017-08-07 20:05:04 +0200

    second solution

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,17 +1,26 @@
 <?php
-
-for ($i = 1; $i <= 100; $i++)
+function simpleFizzBuzz($number = 100)
 {
-    if($i % 3 == 0 && $i % 5 ==0){
-        echo "FizzBuzz<br />";
-    }
-    else if($i % 3 == 0){
-        echo "Fizz<br />";
-    }
-    else if($i % 5 == 0){
-        echo "Buzz<br />";
-    }
-    else {
-        echo $i."<br />";
+    for ($i = 1; $i <= $number; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo "FizzBuzz<br />";
+        } else if ($i % 3 == 0) {
+            echo "Fizz<br />";
+        } else if ($i % 5 == 0) {
+            echo "Buzz<br />";
+        } else {
+            echo $i . "<br />";
+        }
     }
 }
+
+function secondFizzBuzz(){
+    for ($i = 1; $i <= 100; $i++) {
+        $mod3 = $i % 3;
+        $mod5 = $i % 5;
+        echo (!$mod3 && !$mod5 ? "FizzBuzz" : (!$mod3 ? "Fizz" : (!$mod5 ? "Buzz" : $i))) ."<br>";
+    }
+}
+
+//simpleFizzBuzz(30);
+//secondFizzBuzz();
